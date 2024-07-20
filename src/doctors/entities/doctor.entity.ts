98b565224f3aabe6bddd85c1620cee6e0a3c5c7f
tshadz <thais.sombra@hotmail.com>
2024-07-20 08:18:57 -0300

    first commit

---
+++ src/doctors/entities/doctor.entity.ts
@@ -0,0 +1,7 @@
+export class Doctor {
+  name: string = 'Doctor';
+  specialization: string = 'General';
+  crm: string = 'CRM-SP-XXX.XXX';
+  email: string = 'doctor@example.com';
+  password: string = 'password-examples';
+}
